b9ea3dbe4fb5e997ffd1063e8b1b6f80006f531a
Gabriel Toledo Zucoloto <gabriel.zucoloto@itixti.com.br>
2020-06-07 10:21:48 -0300

    Basic mimic.py solved

--- basic/list1.py
+++ basic/list1.py
@@ -45,7 +45,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    return sorted(tuples, key=lambda t: t[-1]) # Python is astounding!
+    return sorted(tuples, key=lambda t: t[-1])  # Python is astounding!
 
 
 # Simple provided test() function used in main() to print
